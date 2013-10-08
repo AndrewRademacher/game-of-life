3c3453a0c25c628635528bd1e430a6fee457ae9a
Andrew Rademacher <andrewrademacher@gmail.com>
2013-10-08 18:12:31 -0500

    Added basic display main.

--- Main.hs
+++ Main.hs
@@ -7,19 +7,29 @@ import           System.Console.CmdArgs
 import           Data.Text                              as T
 import           Data.Text.IO                           as TIO
 import           Data.Array.Repa                        as R
-import           Data.Array.Repa.Algorithms.Randomish   as Ar
+import           Data.Array.Repa.Algorithms.Randomish   as AR
+import           Graphics.Gloss
 
-data Life = Life { boardWidth  :: Int
-                 , cellWidth   :: Int
-                 , generations :: Int }
+type Board = Array U DIM2 Int
+data Life = Life { width     :: Int
+                 , height    :: Int
+                 , cellWidth :: Int
+                 , genDelay  :: Float }
             deriving (Show, Data, Typeable)
 
-argsLife = Life { boardWidth    = 50  &= help "The number of cells across the game board."
+argsLife = Life { width         = 50  &= help "The number of cells across the game board."
+                , height        = 50  &= help "The number of cells tall."
                 , cellWidth     = 10  &= help "The number of pixels across a single cell."
-                , generations   = 100 &= help "The number of generations to simulate." }
+                , genDelay      = 1   &= help "The number of seconds between each generation." }
 
 main :: IO ()
---main = P.print =<< cmdArgs argsLife
 main = do
-        args <- cmdArgs argsLife
-        P.print args
+        life <- cmdArgs argsLife
+        let board         = randomishIntArray (Z :. (width life) :. (height life)) 0 1 1
+            displayWidth  = (cellWidth life) * (width life)
+            displayHeight = (cellWidth life) * (height life)
+        display (InWindow "Game of Life" (displayWidth, displayHeight) (10, 10))
+                white (picture life board)
+
+picture :: Life -> Board -> Picture
+picture = undefined
