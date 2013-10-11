87256fb5fb1546a790b077ac27d5c1c9643d1dc1
Andrew Rademacher <andrewrademacher@gmail.com>
2013-10-10 19:56:07 -0500

    Added a simulation model.

--- Main.hs
+++ Main.hs
@@ -11,52 +11,67 @@ import           Data.List                              as L
 import           Data.Vector                            as V
 import           Data.Vector.Unboxed                    as U
 import           Graphics.Gloss
+import           Graphics.Gloss.Data.ViewPort
 
-type Board = V.Vector Int
-data Life = Life { width     :: Int
-                 , height    :: Int
-                 , cellWidth :: Int
-                 , genDelay  :: Float }
+data Life = Life { width_     :: Int
+                 , height_    :: Int
+                 , cellWidth_ :: Int
+                 , genPerSec_ :: Int }
             deriving (Show, Data, Typeable)
 
-argsLife = Life { width         = 50  &= help "The number of cells across the game board."
-                , height        = 50  &= help "The number of cells tall."
-                , cellWidth     = 10  &= help "The number of pixels across a single cell."
-                , genDelay      = 1   &= help "The number of seconds between each generation." }
+argsLife = Life { width_         = 50  &= help "The number of cells across the game board."
+                , height_        = 50  &= help "The number of cells tall."
+                , cellWidth_     = 10  &= help "The number of pixels across a single cell."
+                , genPerSec_     = 1   &= help "The number of generations per second." }
+
+type Board = V.Vector Int
+data Generation = Generation { width     :: Int
+                             , height    :: Int
+                             , cellWidth :: Float
+                             , genPerSec :: Int
+                             , board     :: Board }
 
 main :: IO ()
 main = do
         life <- cmdArgs argsLife
         seed <- newStdGen
-        let board = randomBoard (width life) (height life) seed
-            displayWidth  = (cellWidth life) * (width life)
-            displayHeight = (cellWidth life) * (height life)
-        display (InWindow "Game of Life" (displayWidth, displayHeight) (10, 10))
-                white (pictureBoard life board displayWidth displayHeight)
+        let firstGen      = makeFirstGen life seed
+            displayWidth  = (cellWidth_ life) * (width_ life)
+            displayHeight = (cellWidth_ life) * (height_ life)
+        simulate (InWindow "Game of Life" (displayWidth, displayHeight) (10, 10))
+            white (genPerSec firstGen) firstGen pictureGeneration nextGeneration
+
+makeFirstGen :: Life -> StdGen -> Generation
+makeFirstGen (Life w h cw gps) seed = 
+        Generation w h (fromIntegral cw :: Float) gps (randomBoard w h seed)
 
 randomBoard :: Int -> Int -> StdGen -> Board
 randomBoard w h = V.take (w * h) . V.unfoldr (Just . randomR (0, 1))
 
-getCoords :: Life -> Int -> (Int, Int)
-getCoords life idx = (x, y)
-    where x  = idx `mod` (width life)
-          y  = idx `div` (width life)
+getCoords :: Generation -> Int -> (Int, Int)
+getCoords gen idx = (x, y)
+    where x  = idx `mod` (width gen)
+          y  = idx `div` (width gen)
 
-fromCoords :: Life -> (Int, Int) -> Int
-fromCoords life (x, y) = x + (y * (width life))
+fromCoords :: Generation -> (Int, Int) -> Int
+fromCoords gen (x, y) = x + (y * (width gen))
 
-pictureBoard :: Life -> Board -> Int -> Int -> Picture
-pictureBoard life board dw dh   = Translate tx ty
-                                $ Scale cw cw 
-                                $ Pictures (V.toList (V.imap (pictureCell life) board))
-    where cw = (fromIntegral (cellWidth life) :: Float)
-          tx = (fromIntegral (0 - (dw `div` 2)) :: Float)
-          ty = (fromIntegral (0 - (dh `div` 2)) :: Float)
+pictureGeneration :: Generation -> Picture
+pictureGeneration gen   = Translate tx ty
+                        $ Scale (cellWidth gen) (cellWidth gen)
+                        $ Pictures (V.toList (V.imap (pictureCell gen) (board gen)))
+    where tx = 0 - (((cellWidth gen) * w) / 2)
+          ty = 0 - (((cellWidth gen) * h) / 2)
+          w  = (fromIntegral (width gen) :: Float)
+          h  = (fromIntegral (height gen) :: Float)
 
-pictureCell :: Life -> Int -> Int -> Picture
-pictureCell life idx state  = Translate (fromIntegral x :: Float) (fromIntegral y :: Float)
+pictureCell :: Generation -> Int -> Int -> Picture
+pictureCell gen idx state  = Translate (fromIntegral x :: Float) (fromIntegral y :: Float)
                             $ Color (color state)
                             $ Polygon [(0, 0), (0, 1), (1, 1), (1, 0)]
-    where (x, y)  = getCoords life idx
+    where (x, y)  = getCoords gen idx
           color 0 = makeColor 1 1 1 1
           color 1 = makeColor 0 0 0 1
+
+nextGeneration :: ViewPort -> Float -> Generation -> Generation
+nextGeneration view time lastGen = lastGen
