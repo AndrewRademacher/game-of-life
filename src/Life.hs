8865e48d8929649afd56c60d72804312eacbbda3
Andrew Rademacher <andrewrademacher@gmail.com>
2013-10-15 23:12:45 -0500

    Enfoced strict evaluation and inlining of nextGen.

--- src/Life.hs
+++ src/Life.hs
@@ -19,18 +19,19 @@ randomGen width height gen = randomishIntArray (Z :. width :. height :: DIM2) 0 
     where (s, _) = (random gen :: (Int, StdGen))
 
 nextGen :: Generation -> Generation
-nextGen lastGen = runIdentity . computeP $ traverse lastGen id (nextCell (extent lastGen))
+nextGen !lastGen = runIdentity . computeP $ traverse lastGen id (nextCell (extent lastGen))
 
 nextCell :: DIM2 -> (DIM2 -> Int) -> DIM2 -> Int
-nextCell aSh lkp loc@(Z :. x :. y)  |  nc < 2 || nc > 3  =  0
-                                    |  nc == 3           =  1
-                                    |  otherwise         =  lkp loc
-    where nc        =   (gn n n) + (gn z n) + (gn p n)
+{-# INLINE nextCell #-}
+nextCell !aSh !lkp !loc@(Z :. x :. y) |  nc < 2 || nc > 3  =  0
+                                      |  nc == 3           =  1
+                                      |  otherwise         =  lkp loc
+    where !nc       =   (gn n n) + (gn z n) + (gn p n)
                       + (gn n z) +            (gn p z)
                       + (gn n p) + (gn z p) + (gn p p)
-          gn mx my | inShape aSh nLoc = lkp nLoc
-                   | otherwise        = 0
-            where nLoc = (Z :. x + mx :. y + my)
-          n        = (-1)
-          p        = (1)
-          z        = (0)
+          gn !mx !my | inShape aSh nLoc = lkp nLoc
+                     | otherwise        = 0
+            where !nLoc = (Z :. x + mx :. y + my)
+          !n       = (-1)
+          !p       = (1)
+          !z       = (0)
