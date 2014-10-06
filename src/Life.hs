f66e01ed1a4bd2dd9f7da2325c270f778d909904
AndrewRademacher <andrewrademacher@gmail.com>
2014-10-06 14:58:36 -0500

    Switched from traverse to stencil.

--- src/Life.hs
+++ src/Life.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE BangPatterns #-}
+{-# LANGUAGE QuasiQuotes #-}
 
 module Life
     ( Generation
@@ -7,11 +7,13 @@ module Life
     , nextGen
     ) where
 
-import           Control.Monad.Identity (runIdentity)
-import           Data.Array.Repa        as R
-import           Data.Vector.Unboxed    (Vector)
+import           Control.Monad.Identity       (runIdentity)
+import           Data.Array.Repa              as R
+import           Data.Array.Repa.Stencil
+import           Data.Array.Repa.Stencil.Dim2
+import           Data.Vector.Unboxed          (Vector)
 import           Data.Word
-import           Prelude                hiding (map)
+import           Prelude                      hiding (map)
 import           System.Random.MWC
 
 type Generation    = Array U DIM2 Word8
@@ -23,19 +25,13 @@ randomGen w h = do
         return $ runIdentity . computeP $ map (\a -> if a then 1 else 0) bs
 
 nextGen :: Generation -> Generation
-nextGen !lastGen = runIdentity . computeP $ traverse lastGen id (nextCell (extent lastGen))
+nextGen lg = runIdentity . computeP $ R.zipWith nextCell lg ncs
+    where ncs = mapStencil2 (BoundConst 0) st lg
+          st  = [stencil2| 1 1 1
+                           1 0 1
+                           1 1 1 |]
 
-nextCell :: DIM2 -> (DIM2 -> Word8) -> DIM2 -> Word8
-{-# INLINE nextCell #-}
-nextCell !aSh !lkp !loc@(Z :. x :. y) |  nc < 2 || nc > 3  =  0
-                                      |  nc == 3           =  1
-                                      |  otherwise         =  lkp loc
-    where !nc       =   (gn n n) + (gn z n) + (gn p n)
-                      + (gn n z) +            (gn p z)
-                      + (gn n p) + (gn z p) + (gn p p)
-          gn !mx !my | inShape aSh nLoc = lkp nLoc
-                     | otherwise        = 0
-            where !nLoc = (Z :. x + mx :. y + my)
-          !n       = (-1)
-          !p       = (1)
-          !z       = (0)
+nextCell :: Word8 -> Word8 -> Word8
+nextCell lc nc | nc < 2 || nc > 3 = 0
+               | nc == 3          = 1
+               | otherwise        = lc
